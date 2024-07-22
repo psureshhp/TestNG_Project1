d55fddadad99bd06fa113bb720c5a5d5a9e9d084
psureshhp <psuresh.hp@gmail.com>
2024-07-22 23:44:03 +0530

    commit

---
+++ src/test/java/com/testng/Test1.java
@@ -0,0 +1,11 @@
+package com.testng;
+
+import org.testng.annotations.Test;
+public class Test1 {
+	
+	@Test
+	public void test1() {
+		System.out.println("Test1 from class test1 from TestNG_Framework1 suresh");
+	}
+
+}
